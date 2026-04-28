5c6b6cd26e304cdba38a1d435221cdee2b885569
SirJasirIV <gasser354354@gmail.com>
2026-04-28 19:36:21 +0300

    added specific word choice by player, var assigned to a number<3

--- code.js
+++ code.js
@@ -2,5 +2,18 @@
 function getcomputerchoice(choice){
  choice = Math.floor(Math.random()*3)
  console.log(choice)
-}
-getcomputerchoice()
+};
+// the human picks a word and the function assigns it to a number.//
+function gethumanchoice(Hchoice){
+ let answer = prompt("Choose between rock, paper, and scissors!")
+ if (answer == "rock"){
+ Hchoice = 0;
+ } else if (answer == "paper"){
+    Hchoice = 1;
+ } else if (answer == "scissors"){
+    Hchoice = 2;
+ }   else {
+    console.log("Not a choice!")
+ }
+console.log(Hchoice)
+};
