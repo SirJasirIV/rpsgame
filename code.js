bed2eb2dc55b2cde37bf964edad7a82875b42e19
SirJasirIV <gasser354354@gmail.com>
2026-04-28 19:21:39 +0300

    added random choice by computer

--- code.js
+++ code.js
@@ -1,1 +1,6 @@
-console.log("Hello World!")
+// the computer picks a random number less than 3, so it's between 3 options.//
+function getcomputerchoice(choice){
+ choice = Math.floor(Math.random()*3)
+ console.log(choice)
+}
+getcomputerchoice()
