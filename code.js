b87b590c0a1044d940f326eab0e67c3a0ebfae41
SirJasirIV <gasser354354@gmail.com>
2026-04-28 23:02:54 +0300

    Can't even explain the major changes.

--- code.js
+++ code.js
@@ -1,19 +1,57 @@
 // the computer picks a random number less than 3, so it's between 3 options.//
-function getcomputerchoice(choice){
- choice = Math.floor(Math.random()*3)
- console.log(choice)
-};
+function getcomputerchoice(){
+let choice = Math.floor(Math.random()*3)
+ if (choice == 0){
+    return "rock"
+} else if (choice == 1){
+    return "paper"
+} else if (choice == 2){
+    return "scissors" 
+}
+}; 
 // the human picks a word and the function assigns it to a number.//
-function gethumanchoice(Hchoice){
- let answer = prompt("Choose between rock, paper, and scissors!")
- if (answer == "rock"){
- Hchoice = 0;
- } else if (answer == "paper"){
-    Hchoice = 1;
- } else if (answer == "scissors"){
-    Hchoice = 2;
- }   else {
-    console.log("Not a choice!")
- }
-console.log(Hchoice)
-};
+function playround(Humanchoice,Computerchoice){
+    if (Humanchoice == Computerchoice){
+        return "tie!";
+    } 
+    else if ((Humanchoice == "rock" && Computerchoice == "scissors") ||
+    (Humanchoice == "paper" && Computerchoice == "rock") ||
+    (Humanchoice == "scissors" && Computerchoice == "paper")
+    ){
+        return "you win!";
+    } 
+    else {
+        return "you lose!";
+    } 
+}
+function checkwinner(result,Humanchoice,Computerchoice){
+   if (result =="tie!"){
+    console.log("It's a tie!")
+    console.log(`Your score: ${humanscore}`)
+    console.log(`Computer: ${computerscore}`)
+   } else if (result == "you win!"){
+    console.log(`You Win! ${Humanchoice} beats ${Computerchoice}.`)
+    humanscore = humanscore + 1
+    console.log(`Your score: ${humanscore}`)
+    console.log(`Computer: ${computerscore}`)
+   } else if (result =="you lose!"){
+    console.log(`You lose! ${Computerchoice} beats ${Humanchoice}.`)
+    computerscore = computerscore + 1
+    console.log(`Your score: ${humanscore}`)
+    console.log(`Computer: ${computerscore}`)
+   }
+}
+let humanscore = 0;
+let computerscore= 0;
+for (let i = 0; i < 5; i++) {
+ let Humanchoice = prompt("Choose rock, paper or scissors!");
+ if (!Humanchoice) break;
+  Humanchoice = Humanchoice.toLowerCase();
+ let Computerchoice = getcomputerchoice();
+ const result = playround(Humanchoice,Computerchoice);
+ checkwinner(result, Humanchoice, Computerchoice);
+ console.log(`Your choice: ${Humanchoice}`)
+ console.log(`Computer's choice: ${Computerchoice}`)
+}
+
+
